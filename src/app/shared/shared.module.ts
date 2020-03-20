1ace6cb4efa4800a7c50e3c14c212a33341b6691
Edin Fazlic <edin.fazlic@gmail.com>
2020-03-20 08:53:29 +0100

    Added: simple css spinner

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   imports: [
@@ -10,6 +11,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   declarations: [
     NavbarComponent,
+    SpinnerComponent,
   ],
   exports: [
     NavbarComponent,
